3fc5fcbd983b697c5647bb3941d50d759f05d12c
davidgolubev <davidgolubev1@gmail.com>
2023-07-24 09:41:02 +0300

    did profile and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
-import Layout from "./components/Layout/Layout";
 import HomePage from "./components/HomePage/HomePage";
+import HomeAfterStart from "./components/HomeAfterParking/HomeAfterStart";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Login />}></Route>
-      <Route path="/" element={<Layout />}>
-        <Route path="homePage" element={<HomePage />}></Route>
-      </Route>
+      <Route path="homePage" element={<HomePage />}></Route>
+      <Route path="homePageAfterStart" element={<HomeAfterStart />}></Route>
+      <Route path="Profile" element={<Profile />}></Route>
     </Routes>
   );
 }

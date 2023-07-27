398e8f34fc92adaebb175b4b84310aa507e54c7a
amitbarha <amitbar131@gmail.com>
2023-07-27 10:57:34 +0300

    translate token

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,13 @@ import SoloParking from "./components/SoloParking/SoloParking";
 import FindParking from "./components/FindParking/FindParking";
 import PayingHistory from "./components/PayingHistory/PayingHistory";
 import Onepick from "./components/ChoosenParking/Onepick";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const modeContext = createContext();
 export const gooleAutoLocation = createContext();
 export const CloudinaryContext = createContext();
+export const userDataContext = createContext();
 const croods = {
   lat: "",
   lng: "",
@@ -26,8 +29,24 @@ function App() {
   const [colorMode, setColorMode] = useState("light");
   const [googleLocation, setGoogleLocation] = useState(croods);
   const [cloudinaryImg, setCloudinaryImg] = useState([]);
+  const [userData, setUserData] = useState();
+
+
+  useEffect(() => {
+  if(localStorage.getItem('loggedUser'))
+  {
+      axios
+        .post("http://localhost:5000/user/translateToken",{token: localStorage.getItem('loggedUser') })
+        .then(({ data }) => setUserData(data))
+        .catch((err) => console.log(err.message));
+    ;
+  }
+  },[])
+
+
 
   return (
+    <userDataContext.Provider value={{userData, setUserData}}>
     <modeContext.Provider value={{ colorMode, setColorMode }}>
       <gooleAutoLocation.Provider value={{ googleLocation, setGoogleLocation }}>
         <CloudinaryContext.Provider value={{ cloudinaryImg, setCloudinaryImg }}>
@@ -54,6 +73,7 @@ function App() {
         </CloudinaryContext.Provider>
       </gooleAutoLocation.Provider>
     </modeContext.Provider>
+    </userDataContext.Provider>
   );
 }
 

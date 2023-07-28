af0cde48ccc66be08205467455cba5ea1661a0e5
ofek15 <segal.ofek@ofek.com>
2023-07-28 15:59:56 +0300

    update2friday

--- src/components/ChoosenParking/Onepick.jsx
+++ src/components/ChoosenParking/Onepick.jsx
@@ -5,29 +5,84 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 function Onepick() {
+
+  const[loggedUser,setLoggedUser]=useState()
+
+  const[check,setCheck]=useState(false)
+
   const { parkingId } = useParams();
   console.log("id", parkingId);
   const [parkingData, setParkingData] = useState();
+
+  const[ownerParkingData,setOwnerParkingData]=useState()
+
+
+
   useEffect(() => {
     axios
-      .post("http://localhost:5000/parking/findOneParking", { "_id" : "64c2279a6d95516b7b14f91a" })
+      .post("http://localhost:5000/parking/findOneParking", { _id : parkingId })
       .then(({ data }) => {
         setParkingData(data);
+        setCheck(!check)
       })
       .catch((err) => {
         console.log(err.message);
       });
+
+      if (localStorage.getItem('loggedUser')) {
+        axios
+          .post("http://localhost:5000/user/translateToken", { token: localStorage.getItem('loggedUser') })
+          .then(({ data }) =>
+            setLoggedUser(data),
+          )
+          .catch((err) => console.log(err.message));
+  
+      }
   }, []);
+  useEffect(()=>{
+    if(check){
+      axios
+      .post("http://localhost:5000/user/findUserById", { _id : parkingData?.ownerID })
+      .then(({ data }) => {
+        setOwnerParkingData(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }
+  },[check])
 
   const handleStartParking = ()=>{
+    console.log(ownerParkingData,"ownerparkingdata")
+    console.log(parkingData.ownerID,"blbla")
     const date = new Date()
     const time = [date.getHours(),date.getMinutes()]
     const strTime = time.join(":")
     console.log(strTime);
 
     const payment = {
-
+      token:localStorage.getItem('loggedUser'),
+      parkingId:parkingData._id,
+      parkName:parkingData.parkingName,
+      startTime: strTime,
+      endTime: null,
+      pricePerHour: parkingData.pricePerHour,
+      parkingLocation: parkingData.parkingLocation,
+      phoneToPay: ownerParkingData.phoneNumber,
+      clientPhone: loggedUser.phoneNumber,
+      finalPrice:null
     }
+
+    console.log(payment,"payment")
+
+    axios
+      .post("http://localhost:5000/payment/publishPayment", payment)
+      .then(({ data }) => {
+        console.log("new payment create");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
   return (
     <div id="pickone-container">
